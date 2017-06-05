594e042c8751dd22873b1ef015bf95a3091729f1
Hiep Doan <hiep.dv179@gmail.com>
2017-06-05 14:05:11 +0200

    initiate the project with Spring Boot

---
+++ src/main/java/com/alexthered/me/HackernewsStoryRankingApplication.java
@@ -0,0 +1,12 @@
+package com.alexthered.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackernewsStoryRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackernewsStoryRankingApplication.class, args);
+	}
+}
